8abe7967e0ff45cb06adbcb834a11cc76cb66c3d
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-05-28 22:27:39 +0100

    guest

--- testbot.js
+++ testbot.js
@@ -191,7 +191,7 @@ client.on('message', msg => {
 });
 
 client.on('voiceStateUpdate', (oldMember, member) => {
-  if (member.voiceChannelID == '447233002800676864' && member.joinedTimestamp > Date.now() - 1000){
+  if (member.voiceChannelID == '447233002800676864' && member.joinedTimestamp > Date.now() - 5000){
     var role = client.guilds.get('143058431488557056').roles.find('name', 'Guest');
     member.addRole([role]).catch(console.error);
     logChannel.send('<@'+member.id + '> has joined the server as a guest');
@@ -209,7 +209,7 @@ client.on('guildMemberAdd', member => {
       member.setRoles([guardian, core, operator, hunter]);
       logChannel.send('<@'+member.id + '> has joined the server as a member');
     }
-  }, 5000);
+  }, 2000);
 });
 
 client.on('guildMemberRemove', member => {
