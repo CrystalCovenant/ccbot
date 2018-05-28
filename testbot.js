22feca3a19592eaf088cf7877624c88b20a73f23
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-05-28 23:11:23 +0100

    go go go

--- testbot.js
+++ testbot.js
@@ -192,7 +192,6 @@ client.on('message', msg => {
 
 client.on('voiceStateUpdate', (oldMember, member) => {
   var role = client.guilds.get('143058431488557056').roles.find('name', 'Guest');
-  logChannel.send(oldMember.voiceChannelID);
   if (member.voiceChannelID == '447233002800676864' && member.joinedTimestamp > Date.now() - 5000){
     member.addRole(role).catch(console.error);
     logChannel.send('<@'+member.id + '> has joined the server as a guest');
