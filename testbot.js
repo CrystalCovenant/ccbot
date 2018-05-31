5eddb827b6ab60669ba5fd69e67120530b046759
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-05-31 19:55:35 +0100

    fix

--- testbot.js
+++ testbot.js
@@ -237,7 +237,7 @@ client.on('guildMemberRemove', member => {
 client.on('messageReactionAdd', (reaction, user) => {
   if (reaction.message.id == '451817062831095818'){
     if (reaction.emoji.id == dEmote.id){
-      var role = msg.guild.roles.find('name', 'Guardian');
+      var role = reaction.message.guild.roles.find('name', 'Guardian');
       if (user.roles.has(role.id)){
         botChannel.send('<@'+user.id + '> Already has the Guadian Role.').then(function(message){ message.delete(5000); });
       } else {
