19336ba83970087676729ec59ac5affe17ef1158
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-05-31 20:02:32 +0100

    test add

--- testbot.js
+++ testbot.js
@@ -240,13 +240,11 @@ client.on('messageReactionAdd', (reaction, user) => {
       var role = reaction.message.guild.roles.find('name', 'Guardian');
       botChannel.send('Found Role: ' + role.id);
       if (reaction.message.member.roles.has(role.id)){
-        botChannel.send('remove');
-        // botChannel.send('<@'+user.id + '> Already has the Guadian Role.').then(function(message){ message.delete(5000); });
+        botChannel.send('<@'+user.id + '> Already has the Guadian Role.').then(function(message){ message.delete(5000); });
       } else {
-        botChannel.send('add');
-        // user.addRole(role).catch(console.error);
-        // logChannel.send('<@'+user.id + '> added Guardian role');
-        // botChannel.send('<@'+user.id + '> Guardian role added.').then(function(message){ message.delete(5000); });
+        user.addRole(role).catch(console.error);
+        logChannel.send('<@'+user.id + '> added Guardian role');
+        botChannel.send('<@'+user.id + '> Guardian role added.').then(function(message){ message.delete(5000); });
         //add
       }
     }
