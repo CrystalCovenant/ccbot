7c2b79d747dc5723a7b2b4f7f773a2f9fc21558e
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-05-31 20:00:33 +0100

    t

--- testbot.js
+++ testbot.js
@@ -235,22 +235,20 @@ client.on('guildMemberRemove', member => {
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-  // botChannel.send('Emote added');
   if (reaction.message.id == '451817062831095818'){
     if (reaction.emoji.id == dEmote.id){
-      // botChannel.send('Found Emote: ' + dEmote.id);
       var role = reaction.message.guild.roles.find('name', 'Guardian');
       botChannel.send('Found Role: ' + role.id);
-      // if (user.roles.has(role.id)){
-
-      //   botChannel.send('<@'+user.id + '> Already has the Guadian Role.').then(function(message){ message.delete(5000); });
-      // } else {
-      //   user.addRole(role).catch(console.error);
-      //   logChannel.send('<@'+user.id + '> added Guardian role');
-      //   botChannel.send('<@'+user.id + '> Guardian role added.').then(function(message){ message.delete(5000); });
-      //   //add
-      // }
-      // logChannel.send('<@'+user.id + '> added the destiny role.');
+      if (user.roles.has(role.id)){
+        botChannel.send('remove');
+        // botChannel.send('<@'+user.id + '> Already has the Guadian Role.').then(function(message){ message.delete(5000); });
+      } else {
+        botChannel.send('add');
+        // user.addRole(role).catch(console.error);
+        // logChannel.send('<@'+user.id + '> added Guardian role');
+        // botChannel.send('<@'+user.id + '> Guardian role added.').then(function(message){ message.delete(5000); });
+        //add
+      }
     }
   }
 });
