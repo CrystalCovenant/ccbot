bcbcce93f2ce5ca5a9f57ce1ff4ae0b03207bb30
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-06-01 14:36:11 +0100

    pve channel pos

--- testbot.js
+++ testbot.js
@@ -138,6 +138,7 @@ client.on('message', msg => {
     var pveChannel = client.guilds.get('143058431488557056').channels.get('400364097310556164');
     pveChannel.clone('PvE Fireteam ' +(d2pveChannels.length+1)).then( channel => {
       channel.setParent(pveChannel.parentID).then(() => {
+        console.log(pveChannel.position);
         var pos = pveChannel.position + d2pveChannels.length;
         channel.setPosition(pos);
       });
