feee424879da595836a3314b1e933403f9eb71af
CrystalCovenant <39604789+CrystalCovenant@users.noreply.github.com>
2018-07-25 14:20:27 +0100

    error edited

--- testbot.js
+++ testbot.js
@@ -55,8 +55,8 @@ client.on('ready', () => {
   // botChannel.fetchMessage('451845699693314068').then(msg => { msg.edit("\nBot Commands:\n\n`-inviteguest` - Creates a 24 hour temporary invite to the Discord as a Guest\n`-destinypve` - Creates a new Destiny PvE Voice Channel\n`-destinypvp` - Creates a new Destiny PvP Voice Channel\n`-destinyraid` - Creates a new Destiny Raid Voice Channel\n\n-----------------------------------------------------------------------------------------------------------"); });
   
   botChannel.fetchMessage('451839276058017792').then( message => {
-    // errorChannel.send(message.content)
-    message.edit("Hey "+core.toString()+" !\n\nYou can now add and remove roles based on the games you are interested in.\n\nHow to use:\n\n1. Click the emotes related to the channels you want to see.\n2. Remove any emotes to remove channels from the sidebar.\n3. If you still want to see channels but don't want pings, mute the corresponding channel.\n4. 'Community Hub' and 'Other Games' will always be visible.\n\nKey:\n\n"+`${dEmote}`+" - Destiny 2\n"+`${ubiEmote}`+" - Ubisoft Games\n"+`${mhEmote}`+" - Monster Hunter\n"+`${noManSkyEmote}`+" - No Mans Sky\n\nPlease leave any feedback in "+client.channels.get('388176998025003010').toString()+".")
+    // 
+    message.edit("Hey "+core.toString()+" !\n\nYou can now add and remove roles based on the games you are interested in.\n\nHow to use:\n\n1. Click the emotes related to the channels you want to see.\n2. Remove any emotes to remove channels from the sidebar.\n3. If you still want to see channels but don't want pings, mute the corresponding channel.\n4. 'Community Hub' and 'Other Games' will always be visible.\n\nKey:\n\n"+`${dEmote}`+" - Destiny 2\n"+`${ubiEmote}`+" - Ubisoft Games\n"+`${mhEmote}`+" - Monster Hunter\n"+`${noManSkyEmote}`+" - No Mans Sky\n\nPlease leave any feedback in "+client.channels.get('388176998025003010').toString()+".").then(msg => { errorChannel.send(msg.content)}).catch(err => {errorChannel.send(err)})
   });
 
 
